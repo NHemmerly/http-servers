d437372276f57ab40a1a197ce1623b0874ee651a
Neil Hemmerly <neilhemm0@gmail.com>
2025-03-12 16:05:04 -0400

    write MakeJwt and ValidateJwt
    Needs more tests to see what happens when secrets do not match
    when uuid is not valid,
    expired jwt tokens,
    more

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -2,7 +2,10 @@ package auth
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -20,4 +23,40 @@ func CheckPasswordHash(password, hash string) error {
 		return fmt.Errorf("password and hash do not match: %w", err)
 	}
 	return nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject:   fmt.Sprintf("%v", userID),
+	})
+	jwt, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", fmt.Errorf("could not sign token: %w", err)
+	}
+	return jwt, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, &claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %s", token.Method.Alg())
+		}
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("could not parse: %w", err)
+	}
+	uuidString, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("could not get subject: %w", err)
+	}
+	id, err := uuid.Parse(uuidString)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("could not parse uuid: %w", err)
+	}
+	return id, nil
 }
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,8 +1,13 @@
 package auth
 
-import "testing"
+import (
+	"testing"
+	"time"
 
-func testCompareHashPassword(t *testing.T) {
+	"github.com/google/uuid"
+)
+
+func TestCompareHashPassword(t *testing.T) {
 	password := "thisisatestpassword"
 	hash, err := HashPassword(password)
 	if err != nil {
@@ -10,5 +15,27 @@ func testCompareHashPassword(t *testing.T) {
 	}
 	if err := CheckPasswordHash(password, hash); err != nil {
 		t.Errorf("hash of %s does not match %s hash", password, hash)
+	}
+}
+
+func TestValidateJWT(t *testing.T) {
+	duration, err := time.ParseDuration("10m")
+	if err != nil {
+		t.Errorf("duration not parsed")
+	}
+	id, err := uuid.Parse("f1e7d154-05fe-4dae-babd-e805734fe71b")
+	if err != nil {
+		t.Errorf("uuid not parsed")
+	}
+	jwtStr, err := MakeJWT(id, "SecretKey123", duration)
+	if err != nil {
+		t.Errorf("jwtString not made")
+	}
+	rightID, err := ValidateJWT(jwtStr, "SecretKey123")
+	if err != nil {
+		t.Errorf("could not validate jwt")
+	}
+	if rightID != id {
+		t.Errorf("RightID %v does not match original id %v", rightID, id)
 	}
 }
