673f23abd71e5f22a29208f4c76585d0ba42caa0
Neil Hemmerly <neilhemm0@gmail.com>
2025-03-12 23:28:55 -0400

    implement additonal tests for jwt

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -8,34 +8,67 @@ import (
 )
 
 func TestCompareHashPassword(t *testing.T) {
-	password := "thisisatestpassword"
-	hash, err := HashPassword(password)
-	if err != nil {
-		t.Errorf("could not hash password: %s", err)
+	// Testing table
+	var tests = []struct {
+		name     string
+		password string
+		hash     string
+		want     bool
+	}{
+		{"hash works", "thisisatestpassword", "thisisatestpassword", true},
+		{"hash seed different", "ThisIsATestPassword", "sike", false},
 	}
-	if err := CheckPasswordHash(password, hash); err != nil {
-		t.Errorf("hash of %s does not match %s hash", password, hash)
+	// The execution loop
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			hash, err := HashPassword(test.hash)
+			if err != nil {
+				t.Errorf("could not hash password: %s", err)
+			}
+			err = CheckPasswordHash(test.password, hash)
+			if (err != nil) == test.want {
+				t.Errorf("hash of %s does not match %s hash", test.password, hash)
+			}
+		})
 	}
+
 }
 
 func TestValidateJWT(t *testing.T) {
-	duration, err := time.ParseDuration("10m")
-	if err != nil {
-		t.Errorf("duration not parsed")
+	var tests = []struct {
+		name      string
+		duration  string
+		secretKey string
+		fakeKey   string
+		uuid      string
+		want      bool
+	}{
+		{"validation works", "10m", "thistestworks", "thistestworks", "f1e7d154-05fe-4dae-babd-e805734fe71b", true},
+		{"expired key", "1ms", "thistestexpires", "thistestexpires", "f1e7d154-05fe-4dae-babd-e805734fe71b", false},
+		{"wrong secret key", "10m", "correctKey", "wrongKey", "f1e7d154-05fe-4dae-babd-e805734fe71b", false},
 	}
-	id, err := uuid.Parse("f1e7d154-05fe-4dae-babd-e805734fe71b")
-	if err != nil {
-		t.Errorf("uuid not parsed")
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			duration, err := time.ParseDuration(test.duration)
+			if err != nil {
+				t.Errorf("duration not parsed")
+			}
+			id, err := uuid.Parse(test.uuid)
+			if err != nil {
+				t.Errorf("uuid not parsed")
+			}
+			jwtStr, err := MakeJWT(id, test.secretKey, duration)
+			if err != nil {
+				t.Errorf("jwtString not made")
+			}
+			rightID, err := ValidateJWT(jwtStr, test.fakeKey)
+			if (err != nil) == test.want {
+				t.Errorf("could not validate jwt: %s", err)
+			}
+			if (rightID == id) != test.want {
+				t.Errorf("RightID %v does not match original id %v", rightID, id)
+			}
+		})
 	}
-	jwtStr, err := MakeJWT(id, "SecretKey123", duration)
-	if err != nil {
-		t.Errorf("jwtString not made")
-	}
-	rightID, err := ValidateJWT(jwtStr, "SecretKey123")
-	if err != nil {
-		t.Errorf("could not validate jwt")
-	}
-	if rightID != id {
-		t.Errorf("RightID %v does not match original id %v", rightID, id)
-	}
+
 }
